a4557d6fb309f4fa3702e91ea0d1d5d93708b07c
Neville Wadia <ndwadia@gmail.com>
2017-09-29 15:37:13 +0530

    ok

--- ch16/mongoose_remove_one.js
+++ ch16/mongoose_remove_one.js
@@ -1,14 +1,20 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
 var wordSchema = require('./word_schema.js').wordSchema;
 var Words = mongoose.model('Words', wordSchema);
+var uri = "mongodb://admin:admin@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/words?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+mongoose.connect(uri, {
+  useMongoClient: true,
+}, function (err, db) {
+  console.log('Connected');
+});
 mongoose.connection.once('open', function(){
-  var query = Words.findOne().where('word', 'unhappy');
+  var query = Words.findOne().where('word', 'gratifactions');
   query.exec(function(err, doc){
     console.log("Before Delete: ");
     console.log(doc);
     doc.remove(function(err, deletedDoc){
-      Words.findOne({word:'unhappy'}, function(err, doc){
+      Words.findOne({word:'gratifactions'}, function(err, doc){
         console.log("\nAfter Delete: ");
         console.log(doc);
         mongoose.disconnect();
--- ch16/mongoose_update_many.js
+++ ch16/mongoose_update_many.js
@@ -1,14 +1,20 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
 var wordSchema = require('./word_schema.js').wordSchema;
 var Words = mongoose.model('Words', wordSchema);
+var uri = "mongodb://admin:admin@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/words?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+mongoose.connect(uri, {
+  useMongoClient: true,
+}, function (err, db) {
+  console.log('Connected');
+});
 mongoose.connection.once('open', function(){
   Words.find({word:/grati.*/}, function(err, docs){
     console.log("Before update: ");
     for (var i in docs){
       console.log(docs[i].word + " : " + docs[i].size);
     }
-    var query = Words.update({}, {$set: {size: 0}});
+    var query = Words.update({}, {$set: {size: 13}});
     query.setOptions({multi: true});
     query.where('word').regex(/grati.*/);
     query.exec(function(err, results){
