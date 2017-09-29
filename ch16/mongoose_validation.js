30fbf058f9aa1a500cc04b86bf202cbc7d38ef0b
Neville Wadia <ndwadia@gmail.com>
2017-09-29 17:12:26 +0530

    ok

--- ch16/mongoose_validation.js
+++ ch16/mongoose_validation.js
@@ -1,19 +1,30 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
 var wordSchema = require('./word_schema.js').wordSchema;
 var Words = mongoose.model('Words', wordSchema);
-Words.schema.path('word').validate(function(value){
+var uri = "mongodb://admin:admin@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/words?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+mongoose.connect(uri, {
+  useMongoClient: true,
+}, function (err, db) {
+  console.log('Connected');
+  setTimeout(function () {
+    mongoose.disconnect();
+  }, 3000);
+});
+
+Words.schema.path('word').validate(function (value) {
   return value.length > 0;
 }, "Word is Too Small");
-Words.schema.path('word').validate(function(value){
+Words.schema.path('word').validate(function (value) {
   return value.length < 20;
 }, "Word is Too Big");
-mongoose.connection.once('open', function(){
+
+mongoose.connection.once('open', function () {
   var newWord = new Words({
-    word:'supercalifragilisticexpialidocious',
-    first:'s',
-    last:'s',
-    size:'supercalifragilisticexpialidocious'.length,
+    word: 'supercalifragilisticexpialidocious',
+    first: 's',
+    last: 's',
+    size: 'supercalifragilisticexpialidocious'.length,
   });
   newWord.save(function (err) {
     console.log(err.errors.word.message);
