97242c89fc4d5e7975b2edc894d8a1f13d3ff820
Neville Wadia <ndwadia@gmail.com>
2017-09-29 10:11:52 +0530

    ok

--- ch16/mongoose_save.js
+++ ch16/mongoose_save.js
@@ -1,19 +1,39 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/words');
 var wordSchema = require('./word_schema.js').wordSchema;
 var Words = mongoose.model('Words', wordSchema);
-mongoose.connection.once('open', function(){
-  var query = Words.findOne().where('word', 'book');
-  query.exec(function(err, doc){
+var uri = "mongodb://admin:admin@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/words?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+mongoose.connect(uri, {
+  useMongoClient: true,
+}, function (err, db) {
+  console.log('Connected');
+});
+
+mongoose.connection.once('open', function () {
+  var query = Words.findOne().where('word', 'boik');
+  query.exec(function (err, doc) {
     console.log("Is Document New? " + doc.isNew);
     console.log("\nBefore Save: ");
     console.log(doc.toJSON());
-    doc.set('word','Book');
-    doc.set('first','B');
+    doc.set('word', 'book');
+    doc.set('letters', ['b', 'o', 'k']);
+    doc.set('charsets','');
     console.log("\nModified Fields: ");
     console.log(doc.modifiedPaths());
-    doc.save(function(err){
-      Words.findOne({word:'Book'}, function(err, doc){
+    doc.save(function (err) {
+      if (err) {
+        console.log(err.message);
+        mongoose.disconnect();
+        return;
+      }
+      Words.findOne({
+        word: 'book'
+      }, function (err, doc) {
+        if (err) {
+          console.log(err.message);
+          mongoose.disconnect();
+          return;
+        }
         console.log("\nAfter Save: ");
         console.log(doc.toJSON());
         mongoose.disconnect();
