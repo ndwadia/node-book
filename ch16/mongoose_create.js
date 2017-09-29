82cba713999ddbaf377fd75f8e269a9981f149b2
Neville Wadia <ndwadia@gmail.com>
2017-09-29 08:29:26 +0530

    ok

--- ch16/mongoose_create.js
+++ ch16/mongoose_create.js
@@ -11,25 +11,25 @@ mongoose.connect(uri, {
 
 mongoose.connection.once('open', function(){
   var newWord1 = new Words({
-    word:'gratifaction',
+    word:'gracifaction',
     first:'g', last:'n', size:12,
-    letters: ['g','r','a','t','i','f','c','o','n'],
+    letters: ['g','r','a','c','i','f','c','o','n'],
     stats: {vowels:5, consonants:7}
   });
   console.log("Is Document New? " + newWord1.isNew);
   newWord1.save(function(err, doc){
     console.log("\nSaved document: " + doc);
   });
-  var newWord2 = { word:'googled',
-    first:'g', last:'d', size:7,
+  var newWord2 = { word:'gooogled',
+    first:'g', last:'d', size:8,
     letters: ['g','o','l','e','d'],
-    stats: {vowels:3, consonants:4}
+    stats: {vowels:4, consonants:4}
   };
   var newWord3 = {
-    word:'selfie',
-    first:'s', last:'e', size:6,
-    letters: ['s','e','l','f','i'],
-    stats: {vowels:3, consonants:3}
+    word:'selfiefy',
+    first:'s', last:'e', size:8,
+    letters: ['s','e','l','f','i','y'],
+    stats: {vowels:3, consonants:5}
   };
   Words.create([newWord2, newWord3], function(err){
     for(var i=1; i<arguments.length; i++){
