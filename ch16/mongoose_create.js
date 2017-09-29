60b4fef72cf732edb1f3cd6ecf07f291380ee44e
Neville Wadia <ndwadia@gmail.com>
2017-09-29 08:57:10 +0530

    ok

--- ch16/mongoose_create.js
+++ ch16/mongoose_create.js
@@ -9,31 +9,47 @@ mongoose.connect(uri, {
   console.log('Connected');
 });
 
-mongoose.connection.once('open', function(){
-  var newWord1 = new Words({
-    word:'gracifaction',
-    first:'g', last:'n', size:12,
-    letters: ['g','r','a','c','i','f','c','o','n'],
-    stats: {vowels:5, consonants:7}
+mongoose.connection.once('open', function () {
+  var newWord1 = {
+    word: 'gracifaction',
+    first: 'g',
+    last: 'n',
+    size: 12,
+    letters: ['g', 'r', 'a', 'c', 'i', 'f', 'c', 'o', 'n'],
+    stats: {
+      vowels: 5,
+      consonants: 7
+    }
+  };
+  var newWord2 = {
+    word: 'gooogled',
+    first: 'g',
+    last: 'd',
+    size: 8,
+    letters: ['g', 'o', 'l', 'e', 'd'],
+    stats: {
+      vowels: 4,
+      consonants: 4
+    }
+  };
+  var newWord3 = new Words({
+    word: 'selfiefied',
+    first: 's',
+    last: 'd',
+    size: 10,
+    letters: ['s', 'e', 'l', 'f', 'i', 'd'],
+    stats: {
+      vowels: 5,
+      consonants: 5
+    }
   });
-  console.log("Is Document New? " + newWord1.isNew);
-  newWord1.save(function(err, doc){
+  console.log("Is Document New? " + newWord3.isNew);
+  newWord3.save(function (err, doc) {
     console.log("\nSaved document: " + doc);
   });
-  var newWord2 = { word:'gooogled',
-    first:'g', last:'d', size:8,
-    letters: ['g','o','l','e','d'],
-    stats: {vowels:4, consonants:4}
-  };
-  var newWord3 = {
-    word:'selfiefy',
-    first:'s', last:'e', size:8,
-    letters: ['s','e','l','f','i','y'],
-    stats: {vowels:3, consonants:5}
-  };
-  Words.create([newWord2, newWord3], function(err){
-    for(var i=1; i<arguments.length; i++){
-      console.log("\nCreated document: " + arguments[i]);      
+  Words.create([newWord1, newWord2], function (err) {
+    for (var i = 1; i < arguments.length; i++) {
+      console.log("\nCreated document: " + arguments[i]);
     }
     mongoose.disconnect();
   });
