665d9802111f605e029d2ac2095eada8163a7721
Neville Wadia <ndwadia@gmail.com>
2017-09-30 19:03:57 +0530

    ok

--- ch16/mongoose_middleware.js
+++ ch16/mongoose_middleware.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var wordSchema = require('./word_schema.js').wordSchema;
 var uri = "mongodb://admin:admin@cluster0-shard-00-00-5mlyc.mongodb.net:27017,cluster0-shard-00-01-5mlyc.mongodb.net:27017,cluster0-shard-00-02-5mlyc.mongodb.net:27017/words?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 mongoose.connect(uri, {
@@ -9,6 +8,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected');
+  var wordSchema = require('./word_schema.js').wordSchema;
   var Words = db.model('Words', wordSchema);
   console.log("\nCreating new doc: ");
   var newWord = new Words({
