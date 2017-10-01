b26a814c5d88a02de15daed0216b12c9f2596c2d
Neville Wadia <ndwadia@gmail.com>
2017-10-01 11:54:10 +0530

    ok

--- ch18/express_request.js
+++ ch18/express_request.js
@@ -6,13 +6,13 @@ app.get('/user/:userid', function (req, res) {
   console.log("Protocol:  " + req.protocol);
   console.log("IP:\t   " + req.ip);
   console.log("Path:\t   " + req.path);
-  console.log("Host:\t   " + req.host);
+  console.log("Host:\t   " + req.hostname);
   console.log("Method:\t   " + req.method);
   console.log("Query:\t   " + JSON.stringify(req.query));
   console.log("Fresh:\t   " + req.fresh);
   console.log("Stale:\t   " + req.stale);
   console.log("Secure:\t   " + req.secure);
-  console.log("UTF8:\t   " + req.acceptsCharset('utf8'));
+  console.log("UTF8:\t   " + req.acceptsCharsets('utf8'));
   console.log("Connection: " + req.get('connection'));
   console.log("Headers: " + JSON.stringify(req.headers,null,2));
   res.send("User Request");
