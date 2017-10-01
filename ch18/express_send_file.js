30e979b9741254330e89ebf5d23022adc44e3668
Neville Wadia <ndwadia@gmail.com>
2017-10-01 18:07:57 +0530

    ok

--- ch18/express_send_file.js
+++ ch18/express_send_file.js
@@ -1,14 +1,18 @@
 var express = require('express');
-var url = require('url');
 var app = express();
 app.listen(80);
-app.get('/image', function (req, res) {
-  res.sendfile('arch.jpg', 
-               { maxAge: 24*60*60*1000,
-                 root: './views/'},
-               function(err){
-    if (err){
+app.get('/image', function (req, res, nxt) {
+  res.sendFile('arch.jpg', {
+    maxAge: 24 * 60 * 60 * 1000,
+    root: './ch18/views/',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }, function (err) {
+    if (err) {
       console.log("Error");
+      nxt(err);
     } else {
       console.log("Success");
     }
