f238b9530c1edb7921d93d78a122809b248a0717
Neville Wadia <ndwadia@gmail.com>
2017-10-03 22:04:23 +0530

    ok

--- ch24/node_server.js
+++ ch24/node_server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('./static')).
-    use('/images', express.static( '../images')).
-    use('/lib', express.static( '../lib'));
+app.use('/', express.static('./ch24/static')).
+    use('/images', express.static( './images')).
+    use('/lib', express.static( './lib'));
 app.listen(80);
