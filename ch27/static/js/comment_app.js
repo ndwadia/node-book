d7ab6b244b94c944f617067e0783a66df8f5f5e8
Neville Wadia <ndwadia@gmail.com>
2017-10-09 14:50:53 +0530

    ok

--- ch27/static/js/comment_app.js
+++ ch27/static/js/comment_app.js
@@ -84,7 +84,7 @@ app.controller('pageController', ['$scope', '$http','commentSrv',
        commentSrv.getComment($scope.page.commentId, 
                              function(err, comment){
          if(err){
-           $srope.commentThread = {};
+           $srcope.commentThread = {};
          } else {
            $scope.commentThread = comment;
          }
