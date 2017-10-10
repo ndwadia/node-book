2d8531273ba3748423ac82be28bcdfe12312c3f2
Neville Wadia <ndwadia@gmail.com>
2017-10-10 20:13:40 +0530

    ok

--- ch29/controllers/weather_controller.js
+++ ch29/controllers/weather_controller.js
@@ -1,39 +1,44 @@
 var http = require('http');
-function toFahrenheit(temp){
-  return Math.round((temp-273.15)*9/5+32);
+
+function toFahrenheit(temp) {
+  return Math.round((temp - 273.15) * 9 / 5 + 32);
 }
+
 function parseWeather(req, res, weatherResponse) {
   var weatherData = '';
+  var wdata;
   weatherResponse.on('data', function (chunk) {
     weatherData += chunk;
   });
   weatherResponse.on('end', function () {
     var wObj = JSON.parse(weatherData);
-    if (wObj.name){
-      var wData = {
+    if (wObj.name) {
+      wData = {
         name: wObj.name,
         temp: toFahrenheit(wObj.main.temp),
         tempMin: toFahrenheit(wObj.main.temp_min),
         tempMax: toFahrenheit(wObj.main.temp_max),
         humidity: wObj.main.humidity,
-        wind: Math.round(wObj.wind.speed*2.23694), //mph
+        wind: Math.round(wObj.wind.speed * 2.23694), //mph
         clouds: wObj.clouds.all,
         description: wObj.weather[0].main,
         icon: wObj.weather[0].icon
       };
     } else {
-      wObj = {name: "Not Found"};
+      wObj = {
+        name: "Not Found"
+      };
     }
-    res.json(wData);
+    res.status(200).json(wData);
   });
 }
-exports.getWeather = function(req, res){
-  var city = req.query.city;
+exports.getWeather = function (req, res) {
+  var city = encodeURI(req.query.city);
   var options = {
     host: 'api.openweathermap.org',
-    path: '/data/2.5/weather?q=' + city
+    path: '/data/2.5/weather?appid=eabc7845da19a377179baca9f50ffa59&q=' + city
   };
-  http.request(options, function(weatherResponse){
+  http.request(options, function (weatherResponse) {
     parseWeather(req, res, weatherResponse);
   }).end();
 }
--- ch29/controllers/words_controller.js
+++ ch29/controllers/words_controller.js
@@ -1,34 +1,42 @@
 var mongoose = require('mongoose'),
-    Word = mongoose.model('Word');
-exports.getWords = function(req, res) {
-  var sort = getSortObj(req);
+  Word = mongoose.model('Word');
+exports.getWords = function (req, res) {
+  var sortField = getSortField(req);
   var query = Word.find();
-  if(req.query.contains.length > 0){
-    query.find({'word' : new RegExp(req.query.contains, 'i')});
+  var skipNum = req.query.skip * 1;
+  var limitNum = req.query.limit * 1;
+  var containsStr = req.query.contains;
+  if (containsStr.length > 0) {
+    query.find({
+      'word': new RegExp(req.query.contains, 'i')
+    });
   }
-  query.sort(sort)
-  .limit(req.query.limit)
-  .skip(req.query.skip)
-  .exec(function(err, word) {
-    if (!word){
-      res.json(404, {msg: 'Word Not Found.'});
-    } else {
-      res.json(word);
-    }
-  });
+  query.sort(sortField)
+    .skip(skipNum)
+    .limit(limitNum)
+    .exec(function (err, word) {
+      if (!word) {
+        res.status(404).json({
+          msg: 'Word Not Found.'
+        });
+      } else {
+        res.status(200).json(word);
+      }
+    });
 };
-function getSortObj(req){
+
+function getSortField(req) {
   var field = "word";
-  if(req.query.sort == 'Vowels'){
+  if (req.query.sort == 'Vowels') {
     field = 'stats.vowels';
-  } else if(req.query.sort == 'Consonants'){
+  } else if (req.query.sort == 'Consonants') {
     field = 'stats.consonants';
-  } else if(req.query.sort == 'Length'){
+  } else if (req.query.sort == 'Length') {
     field = 'size';
-  }else{
-    field = req.query.sort.toLowerCase();
+  } else {
+    field = req.query.sort;
+    field = field.toLowerCase();
   }
-  var sort = new Object();
-  sort[field] = req.query.direction;
-  return sort;
-};
+  if (req.query.direction !== 'asc') field = '-' + field;
+  return field;
+}
